aa1c5e5da6c180bf3e08189cfe38d139822aecdc
Christine Movius <christine.movius@gmail.com>
2016-09-05 12:12:54 -0400

    Configure nested routes from Single and PhotoGrid using react-router.

---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Single = React.createClass({
+  render() {
+    return (
+      <div className='single-photo'>
+        I am single photo
+      </div>
+    )
+  }
+});
+
+export default Single;
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-//Import css
+// Import css
 import css from './styles/style.styl';
 
-//Import Components
+// Import Components
 import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 
-render(<Main />, document.getElementById('root'));
+// Import React Router Deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path='/' component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path='/view/:postId' component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'));
