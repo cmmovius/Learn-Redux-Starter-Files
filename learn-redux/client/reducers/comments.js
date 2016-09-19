f268c6019837262c31f6653b59e663f2e71b79d7
Christine Movius <christine.movius@gmail.com>
2016-09-18 21:41:30 -0400

    Create reducers and integrate Store with react router.

---
+++ learn-redux/client/reducers/comments.js
@@ -0,0 +1,8 @@
+// A reducer takes in 1) the action (info about what happened) 2) copy of current state
+
+function comments(state = [], action) {
+  console.log(state, action);
+  return state;
+}
+
+export default comments;
